6c84c9293ffbe40752e79cb6aee1121b9b3806b3
dell <dell@DESKTOP-NU21QA8>
2019-09-30 10:59:36 -0400

    actualizacion

--- practicas/src/cine/Pelicula.java
+++ practicas/src/cine/Pelicula.java
@@ -1,9 +1,67 @@
 package cine;
-
+import java.util.*;
 public class Pelicula {
 
-	public Pelicula() {
-		// TODO Auto-generated constructor stub
+	private String nombre;
+	private String protagonista;
+	private int fechaEstreno;
+	private String genero;
+	
+	
+	public Pelicula(String nombre,String protagonista,int fechaEstreno,String genero) {
+		
+		this.nombre=nombre;
+		this.protagonista=protagonista;
+		this.fechaEstreno=fechaEstreno;
+		this.genero=genero;
 	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getProtagonista() {
+		return protagonista;
+	}
+
+
+	public void setProtagonista(String protagonista) {
+		this.protagonista = protagonista;
+	}
+
+
+	public int getFechaEstreno() {
+		return fechaEstreno;
+	}
+
+
+	public void setFechaEstreno(int fechaEstreno) {
+		this.fechaEstreno = fechaEstreno;
+	}
+
+
+	public String getGenero() {
+		return genero;
+	}
+
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Pelicula [nombre=" + nombre + ", protagonista=" + protagonista + ", fechaEstreno=" + fechaEstreno
+				+ ", genero=" + genero + "]";
+	}
+	
 
 }
